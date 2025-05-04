dc162a3c41e83096242ac2fef7745580faa9ad7f
Taru Wiik <taruwiik@gmail.com>
2025-05-04 20:35:32 +0300

    Imported body-parser module to index.js to parse URL-encoded request bodies, allowing Express to handle form data

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello, express");
